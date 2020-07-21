155d3e12e2bd734bdb5bc9b7863b5054e3d5eeef
AlejoCasti <acastiblanco8@gmail.com>
2020-07-21 20:06:12 +0000

    [FIX]

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,11 +1,9 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_height - fucntion that goes through a binary tree
  * @tree: pointer to root
  * Return: size of the tree
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
